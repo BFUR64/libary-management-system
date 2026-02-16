fb2900191a7a666dffd308cc9c210e1407a29d6f
Terrance1120 <iterranceevan@gmail.com>
2026-02-16 16:52:57 +0800

    Initialized Gradle and proper package structure

---
+++ app/src/main/java/com/teic/library/App.java
@@ -0,0 +1,7 @@
+package com.teic.library;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ app/src/test/java/com/teic/library/AppTest.java
@@ -0,0 +1,10 @@
+package com.teic.library;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class AppTest {
+    @Test public void uselessTest() {
+        Assert.assertTrue(true);
+    }
+}
